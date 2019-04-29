987482bee8fcb552dc826f413d52d9945cc3f8d0
helainepontes <42441279+helainepontes@users.noreply.github.com>
2019-04-29 17:55:08 -0300

    Add files via upload

---
+++ TableViewEstaticEx/TableViewEx/MyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyViewController.swift
+//  TableViewEx
+//
+//  Created by Helaine Alves Pontes on 28/03/2019.
+//  Copyright © 2019 Helaine Alves Pontes. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
